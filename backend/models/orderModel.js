42f0431a1bf98504e72206aba4f344870b5e0e77
Komail Haider <komailhaider010@gmail.com>
2026-05-18 16:01:02 +0500

    Order Schema integrated

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -6,54 +6,23 @@ const orderSchema = new mongoose.Schema({
         ref: 'user',
         required: true,
     },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'course',
+        required: true,
+    },
     amount: {
         type: Number,
         required: true,
     },
-    pric:{
-        type: Number,
-        required: true,
-    },
-    estimatedPrice: {
-        type: Number,
-    },
-    tags: {
-        type: String,
-    },
-    level:{
-        type: String,
-    },
-    thumbnail:{
-        type: String,
-    },
-    demoVideo: {
+    paymentStatus: {
         type: String,
         required: true,
     },
-    benifits:[{
+    stripePaymentIntent: {
         type: String,
-    },],
-    prerequisities:[{
-        type: String,
-    },],
-    reviews:[{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'review',
-    },],
-    chapters:[{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'chapter',
-    }],
-    enroll:[{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',
-    },],
-    ratings:{
-        type: Number,
-        default: 0,
-    },
-
+    }
 }, {timestamps: true});
 
-const Course = new mongoose.model("order", courseSchema);
-module.exports = Course;
+const Order = new mongoose.model("order", orderSchema);
+module.exports = Order;
