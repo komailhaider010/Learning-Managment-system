e733cee90af0c027f9b479aa2b4549cacd29770f
Komail Haider <komailhaider010@gmail.com>
2026-06-09 11:14:33 +0500

    Comment Replies get API build

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -1,67 +1,89 @@
-const Chapter = require('../models/chapterModel');
-const Comment = require('../models/commentModel');
-
+const Chapter = require("../models/chapterModel");
+const Comment = require("../models/commentModel");
 
 const addComment = async (req, res) => {
-    const {content} = req.body;
-    const {chapterId} = req.params;
-    const {userId} = req.user;
-    try {
-        if (!content) {
-            return res.status(400).json({message: "Please Required field"});
-        }
-        const chapter = await Chapter.findById(chapterId);
-        if (!chapter) {
-            return res.status(404).json({message: "Chapter Not Found"});
-        }
-
-        const comment = await Comment.create({
-            user: userId,
-            content,
-        })
-        chapter.questions.push(comment);
-        await chapter.save();
-        
-        res.status(200).json({comment, message:"Sucessfully Added Comment"});
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: 'Internal Server Error' });
+  const { content } = req.body;
+  const { chapterId } = req.params;
+  const { userId } = req.user;
+  try {
+    if (!content) {
+      return res.status(400).json({ message: "Please Required field" });
     }
-}
+    const chapter = await Chapter.findById(chapterId);
+    if (!chapter) {
+      return res.status(404).json({ message: "Chapter Not Found" });
+    }
+
+    const comment = await Comment.create({
+      user: userId,
+      content,
+    });
+    chapter.questions.push(comment);
+    await chapter.save();
+
+    res.status(200).json({ comment, message: "Sucessfully Added Comment" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 const addCommentReply = async (req, res) => {
-    const {content} = req.body;
-    const {commentId} = req.params;
-    const {userId} = req.user;
-    console.log(commentId);
-    try {
-        if (!content) {
-            return res.status(400).json({message: "Please Required field"});
-        }
-        const comment = await Comment.findById(commentId);
-        console.log(comment);
-        if (!comment) {
-            return res.status(404).json({message: "Comment Not Found"});
-        }
-
-        const commentCreate = await Comment.create({
-            user: userId,
-            content,
-        })
-        comment.replies.push(commentCreate);
-        await comment.save();
-        
-        res.status(200).json({comment, message:"Sucessfully Added Comment Reply"});
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: 'Internal Server Error' });
+  const { content } = req.body;
+  const { commentId } = req.params;
+  const { userId } = req.user;
+  console.log(commentId);
+  try {
+    if (!content) {
+      return res.status(400).json({ message: "Please Required field" });
     }
-}
+    const comment = await Comment.findById(commentId);
+    console.log(comment);
+    if (!comment) {
+      return res.status(404).json({ message: "Comment Not Found" });
+    }
 
+    const commentCreate = await Comment.create({
+      user: userId,
+      content,
+    });
+    comment.replies.push(commentCreate);
+    await comment.save();
+
+    res
+      .status(200)
+      .json({ comment, message: "Sucessfully Added Comment Reply" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const getSingleComment = async (req, res) => {
+  const { commentId } = req.params;
+  try {
+    const comment = await Comment.findById(commentId)
+      .populate("user", "name avatar")
+      .populate({
+        path: "replies",
+        select: "-replies", //This explicitly excludes the 'replies' field from the sub-comments
+        populate: {
+          path: "user",
+          select: "name avatar",
+        },
+      });
+    if (!comment) {
+      return res.status(404).json({ message: "Comment Not Found" });
+    }
+    res.status(200).json({ comment });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 module.exports = {
-    addComment,
-    addCommentReply,
-}
+  addComment,
+  addCommentReply,
+  getSingleComment,
+};
--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { authenticateUser, authenticateAdmin } = require("../middleware/auth");
-const { addComment, addCommentReply } = require("../controllers/commentController");
+const { addComment, addCommentReply, getSingleComment } = require("../controllers/commentController");
 
 const router = express.Router();
 
 router.post('/api/:chapterId/add-question', authenticateUser, addComment)
 router.post('/api/:commentId/add-reply', authenticateUser, addCommentReply)
-
+router.get('/api/:commentId/get-single-comment', getSingleComment)
 
 module.exports = router
